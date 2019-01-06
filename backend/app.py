6f0079b7322623f6d059760280e2a66a66575127
Lomomn <8727952+Lomomn@users.noreply.github.com>
2019-01-06 21:58:52 +0000

    added .babelrc which allows for axios to be accessed globally, altered nginx conf with upstream vars

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def index():
-    return 'hello world!'
+    return jsonify({"message": "hello world"})
